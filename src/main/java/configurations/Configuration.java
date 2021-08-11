95ec55eff4b846b988c11aef6cd082e4cfc44aca
Anahit <ayvazyana93@gmail.com>
2021-08-11 14:51:11 +0400

    Added usersListTest

--- src/main/java/configurations/Configuration.java
+++ src/main/java/configurations/Configuration.java
@@ -6,6 +6,8 @@ import java.util.Properties;
 
 public class Configuration {
 
+    public static String BASE_URI;
+    public static String ACCESS_TOKEN;
     public static String DB_URL;
     public static String DB_USERNAME;
     public static String DB_PASSWORD;
@@ -15,6 +17,9 @@ public class Configuration {
             Properties prop = new Properties();
             InputStream inputStream = Configuration.class.getClassLoader().getResourceAsStream("configuration.properties");
             prop.load(inputStream);
+            BASE_URI = System.getProperty("base.uri", prop.getProperty("base.uri"));
+            ACCESS_TOKEN = System.getProperty("api.access.token", prop.getProperty("api.access.token"));
+
             DB_URL = System.getProperty("db.url", prop.getProperty("db.url"));
             DB_USERNAME = System.getProperty("db.username", prop.getProperty("db.username"));
             DB_PASSWORD = System.getProperty("db.password", prop.getProperty("db.password"));
